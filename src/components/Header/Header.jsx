5c6eb5619f53123914127ec5835571106042b4da
Djamalayl <djamalayl32@mail.ru>
2023-10-05 15:25:06 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import styles from './header.module.css'
+import React from "react";
+import styles from "./header.module.css";
 
 const Header = () => {
   return (
-    <header className=''>
-        <div className={styles.logo}>Seterra</div>
-        <div className={styles.login}>log in</div>
+    <header className={styles.header}>
+      <div className={styles.logo}>Seterra</div>
+      <div>
+        <div className={styles.login}>Sign In</div>
+        <div className={styles.login}>Sign Up</div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
