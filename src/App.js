6d9ffbdebd8e99a8fd7b427d5bcec63f737b2a58
Djamalayl <djamalayl32@mail.ru>
2023-10-05 15:45:34 +0300

    conflicts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-import './App.css';
 import Header from "./components/Header/Header";
 import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import styles from "./header.module.css";
 
@@ -15,18 +14,3 @@ const Header = () => {
 };
 
 export default Header;
-=======
-import React from 'react'
-import styles from './header.module.css'
-
-const Header = () => {
-  return (
-    <header className=''>
-        <div className={styles.logo}>Seterra</div>
-        <div className={styles.login}>log in</div>
-    </header>
-  )
-}
-
-export default Header
->>>>>>> main
