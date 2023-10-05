bfb4e3bec0799f4e02d65d673d923155e13271d1
Мохаммад Дидаев <mohammaddidaev@MacBook-Air-Mohammad.local>
2023-10-05 16:06:37 +0300

    doing home page with courses

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
-
 import Home from './pages/Home/Home';
 import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-     <SignIn />
-     <SignUp />
-    </div>
-    );
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signIn' element={<SignIn />} />
+      <Route path='/signUp' element={<SignUp />} />
+    </Routes>
+  );
 }
 
 export default App;
